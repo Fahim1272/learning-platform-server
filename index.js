cdd4ac65bc3967b1391ca3001bec37a8d3760560
Fahim1272 <imfahim72@gmail.com>
2022-10-27 15:10:05 +0600

    data included

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors')
+const categories = require('./categories.json');
+const courses = require('./courses.json');
+const { response } = require('express');
 
-app.get('/',(req , res) => {
+app.use(cors())
+
+app.get('/', (req, res) => {
     res.send('News Api Running')
 });
+app.get('/course_categories', (req, res) => {
+    res.send(categories)
+});
 
-app.listen(port, () =>{
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '04') {
+        res.send(courses)
+    } else {
+        const category_course = courses.filter(c => c.category_id === id);
+        res.send(category_course)
+    }
+
+})
+app.get('/courses', (req, res ) => {
+    res.send(courses)
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const Selectedcourse = courses.find(c => c._id === id);
+    res.send(Selectedcourse);
+    console.log(req.params.id);
+})
+
+app.listen(port, () => {
     console.log("SERVER RUNNING");
 })
